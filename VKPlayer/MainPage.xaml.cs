c9815d0dcd0e5aa807c8c32acd0184f913715b57
d.a.golovachev <d.a.golovachev@gmail.com>
2016-05-12 22:32:12 +0600

    first commit

---
+++ VKPlayer/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Phone.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using VK.WindowsPhone.SDK;
+
+
+namespace VKPlayer
+{
+    public sealed partial class MainPage : Page
+    {
+        private List<string> _scope = new List<string>() {VKScope.AUDIO};
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            VKSDK.Initialize("5420524");
+            VKSDK.Authorize(_scope, false, false);
+        }
+
+
+    }
+}
